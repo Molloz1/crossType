37f548294635f9b9a22791b9187f387685759720
Sudais Kawooya <s.kawooya@itms-ug.com>
2026-06-26 09:29:49 +0300

    added generate and migrate automatically

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -3,7 +3,7 @@
 import { LoginForm } from "@/components/login-form"
 import {  Type } from "lucide-react"
 
-export default function LoginPage() {
+export default function Page() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
